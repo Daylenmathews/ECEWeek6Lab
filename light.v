0f2c783d7659ec74ee84ef4a4a0fd7ea9faffe7e
Daylenmathews <151976934+Daylenmathews@users.noreply.github.com>
2025-10-02 15:29:15 -0600

    Update light.v

--- light.v
+++ light.v
@@ -1,1 +1,12 @@
+module light(
+    // Declare downstairs and upstairs input
+    input downstairs,
+    input upstairs,
+    // Declare stair light output
+    output stair_light
+    
+);
+assign stair_light=downstairs^upstairs;
+    // Enter logic equation here
 
+endmodule
