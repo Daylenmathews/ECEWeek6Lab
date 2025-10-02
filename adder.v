0b492862039c63d36575ff697713803ed4cd3844
Daylenmathews <151976934+Daylenmathews@users.noreply.github.com>
2025-10-02 15:28:43 -0600

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,1 +1,15 @@
+module adder(
+    // Declare your A/B inputs
+    input A,
+    input B,
+    // Declare Y output
+    output Y,
+   
+    // Declare carry output 
+    output Carry
+);
+assign Y=A^B;
+assign Carry=A&B;
+    // Enter logic equation here
 
+endmodule
